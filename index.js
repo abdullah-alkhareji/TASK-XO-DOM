868234a8d3babfa9587ab4f11a650501035a900a
abdullah-alkhareji <outsiderandco@gmail.com>
2022-01-12 20:42:02 +0300

    first draft

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 // Pre-made function that will fill the button with its number.
 // First button top left is called 1, last button bottom right is 9
 function fillButton(index, text) {
-  // This function fills the button of the send index
-  document.getElementById(index).innerHTML = text;
+	// This function fills the button of the send index
+	document.getElementById(index).innerHTML = text;
 }
 // pre-made a function. You can use this function to present an alert to say someone wins
 function winningAlert(winner) {
-  confirm(`Horraaay, ${winner} wins!`);
+	confirm(`Horraaay, ${winner} wins!`);
 }
 
 // SAMPLE CODE: This code fills the 1st and 9th button with X and O initially
 // ❗️ Delete this code once you are done testing
-fillButton(1, "X");
-fillButton(9, "O");
+// fillButton(1, "X");
+// fillButton(9, "O");
 
 /**
  *
@@ -22,9 +22,23 @@ fillButton(9, "O");
  * Add your code here, since this is going to be your main function
  * That interacts with the UI
  */
+
+let term = 0;
+const winningState1 = [1, 2, 3];
+const winningState2 = [4, 5, 6];
+const winningState3 = [7, 8, 9];
+const winningState4 = [1, 5, 9];
+const winningState5 = [3, 5, 7];
+const winningState6 = [1, 4, 7];
+const winningState7 = [2, 5, 8];
+const winningState8 = [3, 5, 9];
+
 function clickButton(index) {
-  console.log(`Button number ${index} is clicked`);
-  // Your main code here.
+	console.log(`Button number ${index} is clicked`);
+	// Your main code here.
+	fillButton(index, checkPlayer());
+	checkWinner();
+	term++;
 }
 
 /**
@@ -34,8 +48,13 @@ function clickButton(index) {
  */
 
 // In this function you should check if the player is X or O
+
 function checkPlayer() {
-  // ....
+	if (term % 2 === 0) {
+		return "x";
+	} else {
+		return "o";
+	}
 }
 
 /**
@@ -43,6 +62,35 @@ function checkPlayer() {
  * checkWinner should be a function that checks
  * who is winning and returns the winner
  */
-// function checkWinner
 
-// function restartGame
+function checkWinner() {
+	let isWin =
+		checkWinningState(winningState1) ||
+		checkWinningState(winningState2) ||
+		checkWinningState(winningState3) ||
+		checkWinningState(winningState4) ||
+		checkWinningState(winningState5) ||
+		checkWinningState(winningState6) ||
+		checkWinningState(winningState7) ||
+		checkWinningState(winningState8);
+
+	if (isWin) {
+		winningAlert(checkPlayer());
+		restartGame();
+	}
+}
+function checkWinningState(winningState) {
+	const value1 = document.getElementById(winningState[0]).innerHTML;
+	const value2 = document.getElementById(winningState[1]).innerHTML;
+	const value3 = document.getElementById(winningState[2]).innerHTML;
+	return value1 !== "" && value1 === value2 && value2 === value3;
+}
+
+// winningState = [1, 2, 3]
+
+function restartGame() {
+	for (let i = 1; i <= 9; i++) {
+		fillButton(i, "");
+	}
+	term = -1;
+}
